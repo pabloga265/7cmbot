846256308016c15008287ea144099a6559596d92
pabloga265 <pabloga265@gmail.com>
2022-08-14 01:16:23 +0200

    bot alive

--- pages/api/bot/index.js
+++ pages/api/bot/index.js
@@ -1,3 +1,5 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
 const tmi = require("tmi.js");
 const { parseMessage } = require("./messageParser.js")
 const opts = {
@@ -10,21 +12,43 @@ const opts = {
   ]
 };
 
+let alive = false;
+
 const client = new tmi.client(opts);
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
 client.connect();
 
+
 const botNames = ['bot', '111']
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
+  const response = parseMessage({target, context, msg, self});
 
-  const message = parseMessage({target, context, msg, self});
-  message && client.say(target, message);
+  if(response) {
+    client.say(target, response);
+  }
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
+  alive = true
 }
+
+
+const handler = (req, res) => {
+  try {
+    if (alive) {
+      res.status(200).json({ message: 'alive' })
+    } else {
+      res.status(200).json({ message: 'connecting' })
+    }
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, message: err.message })
+  }
+}
+
+export default handler
+
